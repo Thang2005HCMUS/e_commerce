fcc82e7af877bb1236013a3eb99ae5c6f04e42aa
= <=>
2026-07-13 20:51:33 +0700

    add test

--- auth/src/test/java/com/example/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/example/auth/AuthApplicationTests.java
@@ -3,16 +3,11 @@ package com.example.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Loại trừ cấu hình AutoConfiguration của DB bằng cách gọi trực tiếp qua tên chuỗi (String), 
-// giúp tránh việc phải import các class không tồn tại trong class-path của Test.
-@SpringBootTest(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-})
+@SpringBootTest
 class AuthApplicationTests {
 
     @Test
     void contextLoads() {
-        // Test chạy thành công khi Spring Context load được mà không cần kết nối DB thật
+        // Test chạy thành công khi Spring Context load được với H2 Database
     }
-
 }
