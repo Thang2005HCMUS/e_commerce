81093a83d8f2add4361d678d77bba723c36b0a5b
= <=>
2026-07-13 20:37:10 +0700

    add test

--- auth/src/test/java/com/example/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/example/auth/AuthApplicationTests.java
@@ -1,16 +1,10 @@
-package com.example.auth; // Thay bằng package đúng của bạn
+package com.example.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-    DataSourceAutoConfiguration.class, 
-    HibernateJpaAutoConfiguration.class
-})
+
 class AuthApplicationTests {
 
     @Test
